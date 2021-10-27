a3b245414a2b24476eea8154ad40f7d1e7cf1cfa
dwajsman <danielwajsman@mac.com>
2021-10-27 11:47:55 -0300

    created without.js

--- assertEqual.js
+++ assertEqual.js
@@ -3,9 +3,10 @@ const assertEqual = function(actual, expected) {
   
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    return;
+    return true;
   }
   console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  return false;
 };
 
 // TEST CODE
---
+++ without.js
@@ -0,0 +1,51 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    return true;
+  }
+  return false;
+};
+//compare elements from firstArr to elements from secondArr
+
+function without(firstArr, secondArr) {
+  // create newArr with same items as firstArr
+  const newArr = [];
+
+  firstArr.forEach(element => {
+    newArr.push(element);
+  });
+
+  //console.log("before: " + firstArr);
+
+  //Remove items from newArr that exist on secondArr
+  for (let i = 0; i < newArr.length; i++) {
+    const firstElement = newArr[i];
+    for (let j = 0; j < secondArr.length; j++) {
+      const secondElement = secondArr[j];
+
+      if (firstElement === secondElement) {
+        //console.log ("removed item: " + firstElement);
+        newArr.splice(i, 1)
+      }
+
+    }
+
+  }
+  return newArr;
+  //console.log("new Arr: " + newArr);
+  //console.log("after: " + firstArr);
+
+}
+
+
+without([1, 2, 3], [1]) 
+// => [2, 3]
+
+without(["1", "2", "3"], [1, 2, "3"]) 
+// => ["1", "2"]
+
+//console.log(without([1, 2, 3], [1]))
+//console.log(without(["1", "2", "3"], [1, 2, "3"]))
+
+
