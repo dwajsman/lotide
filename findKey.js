1e4d6b53a39ed7c6a724d1aa66650b78110db592
dwajsman <danielwajsman@mac.com>
2021-10-29 02:29:54 -0300

    add findKeys.js

---
+++ findKey.js
@@ -0,0 +1,41 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return true;
+  }
+  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  return false;
+};
+
+// MY CODE
+const findKey = function(object, callback) {
+
+  //console.log(object, callback)
+
+  for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+      const element = object[key];
+      if (callback(element)) console.log(key);
+      if (callback(element)) return key;
+
+    }
+  }
+
+};
+
+// EXAMPLE
+const myKey = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+
+
+//TEST
+assertEqual(myKey, "noma");
